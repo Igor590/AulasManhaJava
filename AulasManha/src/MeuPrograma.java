7cd822429cc1096bc9cea74154d9ff4dc6786667
igor590 <igor_590@hotmail.com>
2026-03-04 10:15:26 -0300

    Add files via upload
    04/03.

---
+++ AulasManha/src/MeuPrograma.java
@@ -0,0 +1,2 @@
+public class MeuPrograma {
+}
---
+++ AulasManha/src/veiculos/gestaoCarro.java
@@ -0,0 +1,9 @@
+package veiculos;
+
+public class gestaoCarro {
+    Carro gestaoCarro = new Carro();
+
+    public void atribuirCor(String cor){
+        gestaoCarro.cor = cor;
+    }
+}
