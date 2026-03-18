157df14c0da0e5b625b56b1eeda4f2274efaf4dc
igor590 <igor_590@hotmail.com>
2026-03-18 10:00:45 -0300

    Delete AulasManhaJava-main directory

--- AulasManhaJava-main/AulasManha/src/veiculos/gestaoCarro.java
+++
@@ -1,9 +0,0 @@
-package veiculos;
-
-public class gestaoCarro {
-    Carro gestaoCarro = new Carro("Ferrari", "FODA", 2005);
-
-    public void atribuirCor(String cor){
-        gestaoCarro.cor = cor;
-    }
-}
